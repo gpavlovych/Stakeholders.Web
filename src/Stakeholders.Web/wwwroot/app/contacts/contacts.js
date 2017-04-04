13010ccdc960741fb1b554e96f1e7d813ce3f9a6
George Pavlovych <pavlheo@gmail.com>
2017-04-04 16:21:10 +0300

    contacts are live now

--- src/Stakeholders.Web/wwwroot/app/contacts/contacts.js
+++ src/Stakeholders.Web/wwwroot/app/contacts/contacts.js
@@ -11,11 +11,66 @@ angular
                 activetab: 'contacts'
             });
         }
-    ])
+    ]).factory('Contact', ['$resource',
+    function ($resource) {
+        return $resource(
+            '/api/Contacts/:id',
+            null,
+            {
+                'update': { method: 'PUT' }
+            });
+    }])
     .controller('contactsController',
     [
         '$scope',
-        function ($scope) {
-            //TODO
+        'Contact',
+        'dialogService',
+        function ($scope, Contact, dialogService) {
+            $scope.search = "";
+            $scope.switchView = false;
+
+            function refresh() {
+                Contact.query({ start: 0, count: 10, search: $scope.search },
+                    function (contacts) {
+                        $scope.contacts = contacts;
+                    });
+            }
+
+            refresh();
+            $scope.filter = function () {
+                refresh();
+            };
+
+            $scope.editContact = function (id) {
+                $scope.editedContact = Contact.get({ id: id });
+            };
+
+            $scope.closeEditor = function () {
+                $scope.editedContact = null;
+            };
+
+            $scope.saveEditor = function (event) {
+                dialogService.showConfirmationSaveDialog(event,
+                    function () {
+                        $scope.editedContact.$update({ id: $scope.editedContact.id },
+                            function () {
+                                dialogService.showMessageSavedDialog(event, null);
+                                refresh();
+                            });
+                        $scope.editedContact = null;
+                    },
+                    null);
+            };
+
+            $scope.removeContact = function (event, id) {
+                dialogService.showConfirmationDeleteDialog(event,
+                    function () {
+                        $scope.editedContact.$remove({ id: id },
+                            function () {
+                                refresh();
+                            });
+                    },
+                    null);
+            };
         }
     ]);
