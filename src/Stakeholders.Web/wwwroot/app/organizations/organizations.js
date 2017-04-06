4717aa58edd44db1842f2cbce7113bc5853dd969
George Pavlovych <pavlheo@gmail.com>
2017-04-06 15:24:51 +0300

    Organizations page is ready

--- src/Stakeholders.Web/wwwroot/app/organizations/organizations.js
+++ src/Stakeholders.Web/wwwroot/app/organizations/organizations.js
@@ -3,7 +3,7 @@
 angular
     .module('porlaDashboard.organizations', ['ngRoute'])
     .config([
-        '$routeProvider', function ($routeProvider) {
+        '$routeProvider', function($routeProvider) {
             $routeProvider.when('/organizations',
             {
                 templateUrl: 'organizations/organizations.html',
@@ -12,27 +12,75 @@ angular
             });
         }
     ])
-  .factory('Organization', ['$resource',
-    function ($resource) {
-        return $resource(
-            '/api/Organizations/:id',
-            null,
-            {
-                'update': { method: 'PUT' }
-            });
-    }])
-  .controller('organizationsController',
-    [[
+    .factory('Organization',
+    [
+        '$resource',
+        function($resource) {
+            return $resource(
+                '/api/Organizations/:id',
+                null,
+                {
+                    'update': { method: 'PUT' }
+                });
+        }
+    ])
+    .factory('OrganizationType',
+    [
+        '$resource',
+        function($resource) {
+            return $resource(
+                '/api/OrganizationTypes/:id',
+                null,
+                {
+                    'update': { method: 'PUT' }
+                });
+        }
+    ])
+    .factory('OrganizationCategory',
+    [
+        '$resource',
+        function($resource) {
+            return $resource(
+                '/api/OrganizationCategories/:id',
+                null,
+                {
+                    'update': { method: 'PUT' }
+                });
+        }
+    ])
+    .factory('User',
+    [
+        '$resource',
+        function($resource) {
+            return $resource(
+                '/api/ApplicationUsers/:id',
+                null,
+                {
+                    'update': { method: 'PUT' }
+                });
+        }
+    ])
+    .controller('organizationsController',
+    [
         '$scope',
         'Organization',
+        'OrganizationType',
+        'OrganizationCategory',
+        'User',
         'dialogService',
-        function ($scope, Organization, dialogService) {
+        function ($scope, Organization, OrganizationType, OrganizationCategory, User, dialogService) {
             $scope.search = "";
             $scope.switchView = false;
 
             function refresh() {
                 Organization.query({ start: 0, count: 10, search: $scope.search },
                     function (organizations) {
+                        for (var index = 0; index < organizations.length; index++) {
+                            var organization = organizations[index];
+                            organization.type = OrganizationType.get({ id: organization.typeId });
+                            organization.category = OrganizationCategory.get({ id: organization.categoryId });
+                            organization.user = User.get({ id: organization.userId });
+                        }
                         $scope.organizations = organizations;
                     });
             }
@@ -43,18 +91,57 @@ angular
             };
 
             $scope.editOrganization = function (id) {
-                $scope.editedOrganization = Organization.get({ id: id });
+                $scope.editedOrganizationCategories = OrganizationCategory.query(function(result) {
+                    $scope.editedOrganizationCategories = result;
+                });
+                $scope.editedOrganizationTypes = OrganizationType.query(function (result) {
+                    $scope.editedOrganizationTypes = result;
+                });
+                $scope.editedOrganizationUsers = User.query(function (result) {
+                    $scope.editedOrganizationUsers = result;
+                });
+
+                $scope.editedOrganization = Organization.get({ id: id },
+                    function(result) {
+
+                        $scope.editedOrganizationSelectedCategory = OrganizationCategory
+                            .get({ id: result.categoryId },
+                                function(result) {
+                                    $scope.editedOrganizationSelectedCategory = result;
+                                });
+                        $scope.editedOrganizationSelectedType = OrganizationType
+                            .get({ id: result.typeId },
+                                function(result) {
+                                    $scope.editedOrganizationSelectedType = result;
+                                });
+                        $scope.editedOrganizationSelectedUser = User.get({ id: result.userId },
+                            function(result) {
+                                $scope.editedOrganizationSelectedUser = result;
+                            });
+                    });
             };
 
             $scope.closeEditor = function () {
                 $scope.editedOrganization = null;
             };
 
-            $scope.saveEditor = function (event) {
+            $scope.saveEditor = function(event) {
                 dialogService.showConfirmationSaveDialog(event,
-                    function () {
+                    function() {
+                        $scope.editedOrganization.categoryId =
+                            $scope.editedOrganizationSelectedCategory != null
+                            ? $scope.editedOrganizationSelectedCategory.id
+                            : null;
+                        $scope.editedOrganization.typeId =
+                            $scope.editedOrganizationSelectedType != null
+                            ? $scope.editedOrganizationSelectedType.id
+                            : null;
+                        $scope.editedOrganization.userId =
+                            $scope.editedOrganizationSelectedUser != null
+                            ? $scope.editedOrganizationSelectedUser.id
+                            : null;
                         $scope.editedOrganization.$update({ id: $scope.editedOrganization.id },
-                            function () {
+                            function() {
                                 dialogService.showMessageSavedDialog(event, null);
                                 refresh();
                             });
@@ -74,5 +161,4 @@ angular
                     null);
             };
         }
-    ]
     ]);
